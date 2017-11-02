8e5d8e0301242bb7e20a6f2bc9d6d5ac7c7e7958
pkwenda <pkwenda@163.com>
2017-11-02 09:41:09 +0800

    add spring-boot

---
+++ demo/src/main/java/link/newBee/newBee/NewBeeApplication.java
@@ -0,0 +1,12 @@
+package link.newBee.newBee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewBeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewBeeApplication.class, args);
+	}
+}
---
+++ demo/src/main/java/link/newBee/web/HelloController.java
@@ -0,0 +1,10 @@
+package link.newBee.web;
+
+/**
+ * data 2017-11-02   09:40
+ * E-mail   sis.nonacosa@gmail.com
+ *
+ * @author sis.nonacosa
+ */
+public class HelloController {
+}

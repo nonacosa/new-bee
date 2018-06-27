f23edfca96fab8b7529f3bd7d88d6078b832a73b
six.nonacosa <pkwenda@163.com>
2018-06-28 02:48:22 +0800

    UserController

--- demo/src/main/java/link/newBee/Controller/MainController.java
+++ demo/src/main/java/link/newBee/Controller/MainController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 /**
  * data 2017-11-02   09:40
  * E-mail   sis.nonacosa@gmail.com
- *
  * @author sis.nonacosa
  */
 
---
+++ demo/src/main/java/link/newBee/Controller/UserContreller.java
@@ -0,0 +1,9 @@
+package link.newBee.Controller;
+
+/**
+ * data 2018-06-28   02:47
+ * E-mail   sis.nonacosa@gmail.com
+ * @author sis.nonacosa
+ */
+public class UserContreller {
+}

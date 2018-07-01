6dfb4a280416276644138709041654eca4c20abd
six.nonacosa <pkwenda@163.com>
2018-07-01 15:10:33 +0800

    解决npm报错
    npm install之后经常性报错 node-saa 或 sass-loader的错误
    
    解决方法：rm node_model  &&  rebuild node-sass
    
    应该在isuess中提到这个问题

--- bbs-vue/projInit.sh
+++ bbs-vue/projInit.sh
@@ -3,14 +3,11 @@
 # 全局安装rimraf
 echo ">>>> npm install rimraf -g <<<<"
 ##
-npm install rimraf -g
+sudo npm install rimraf -g
 
-# npm安装electron可能会超时的问题
-echo ">>>> echo 'electron_mirror=https://npm.taobao.org/mirrors/electron/' >> ~/.npmrc <<<<"
-#
-echo 'electron_mirror=https://npm.taobao.org/mirrors/electron/' >> ~/.npmrc
+
 
 # 清除缓存并执行依赖安装
 echo ">>>> npm run install:clean <<<<"
 #
-npm run install:clean
+sudo npm run install:clean
--- bbs-vue/src/renderer/components/common/Section.vue
+++ bbs-vue/src/renderer/components/common/Section.vue
@@ -16,27 +16,7 @@
             </small>
           </div>
           <div class="column is-narrow">
-            <!-- 广告位 -->
-            <!-- <div id="carboncontainer" >  
-              <div id="carbon" class="box">
-
-
-                <div id="carbonads">
-                  <span>
-                    <span class="carbon-wrap">
-                      <a href="//srv.carbonads.net/ads/click/x/GTND42QEF6YI6KQ7F6Y4YKQMCTAIT237CVBDVZ3JCWYDE5QUCTBDTK7KC6BIT5QJCVYDTK3EHJNCLSIZ?segment=placement:bulmaio;"
-                        class="carbon-img" target="_blank" rel="noopener">
-                        <img src="//assets.servedby-buysellads.com/p/manage/asset/id/58581" alt="" border="0" height="100" width="130" style="max-width: 130px;">
-                      </a>
-                      <a href="//srv.carbonads.net/ads/click/x/GTND42QEF6YI6KQ7F6Y4YKQMCTAIT237CVBDVZ3JCWYDE5QUCTBDTK7KC6BIT5QJCVYDTK3EHJNCLSIZ?segment=placement:bulmaio;"
-                        class="carbon-text" target="_blank" rel="noopener">Linux Cloud Hosting - 1GB of RAM for $5/mo. Get a server running in seconds.</a>
-                    </span>
-                    <a href="http://carbonads.net/?utm_source=bulmaio&amp;utm_medium=ad_via_link&amp;utm_campaign=in_unit&amp;utm_term=carbon"
-                      class="carbon-poweredby" target="_blank">ads via Carbon</a>
-                  </span>
-                </div>
-              </div>
-            </div> -->
+             
 
           </div>
         </div>

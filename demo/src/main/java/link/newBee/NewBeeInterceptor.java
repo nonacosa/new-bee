68be473cc513562edf433a1d872f7cb5a60f5e1d
six.nonacosa <pkwenda@163.com>
2018-07-01 11:00:02 +0800

    配置LogBack slf4j

--- demo/src/main/java/link/newBee/NewBeeInterceptor.java
+++ demo/src/main/java/link/newBee/NewBeeInterceptor.java
@@ -8,6 +8,7 @@ package link.newBee;
  */
 import link.newBee.Entity.User;
 import link.newBee.annocation.Log;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;

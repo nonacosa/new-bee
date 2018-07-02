3d3177a260f41b013bfd331f69a30780e59d1f56
six.nonacosa <pkwenda@163.com>
2018-07-02 22:18:28 +0800

    为首页增加 `typeWriter` 小功能

--- bbs-vue/src/renderer/components/common/Section.vue
+++ bbs-vue/src/renderer/components/common/Section.vue
@@ -11,8 +11,8 @@
 
             <br>
             <small class="subtitle  content h6">
-              - 世界因你我而
-              <strong>New Bee</strong>
+              <div id="typeWriter" style="float:left"></div>
+              <strong style="float:left">New Bee</strong>
             </small>
           </div>
           <div class="column is-narrow">
@@ -69,6 +69,23 @@ export default {
           });
         }
       });
+    var typeWriterDom = document.getElementById("typeWriter");
+
+    var typewriter = new Typewriter(typeWriterDom, {
+      loop: false,
+      deleteSpeed: 500
+    });
+
+    typewriter
+      .typeString("-")
+      .pauseFor(2500)
+      .typeString(" 世界因我而")
+      .pauseFor(2000)
+      .deleteChars(2)
+      .pauseFor(1000)
+      .typeString("你我而")
+      .pauseFor(1000)
+      .start();
   },
   destroyed() {},
   methods: {}

1612f8be02e52bfb960d41fa1dad862263f42907
six.nonacosa <pkwenda@163.com>
2018-08-03 17:01:16 +0800

    edit script

--- bbs-shell/deploy-docker.sh
+++ bbs-shell/deploy-docker.sh
@@ -1,14 +1,31 @@
-rm /bbs/newbee.zip && rm /bbs/nginx.conf && rm -rf /bbs/newbee/
 
+# discription : new bee deploy script 
+# author      : sis.nonacosa@gmail.com
+
+# del redundant file
+echo "del local file ... "
+if   [ -f "/bbs/nginx.conf" ]
+then
+    rm /bbs/nginx.conf
+elif [ -f "/bbs/newbee.zip" ]
+    rm /bbs/newbee.zip 
+elif [ -d "/bbs/newbee/" ]  
+    rm /bbs/newbee/
+fi
+
+# cp need deploy's  file --> nodeJs build
+echo "cp newbee.zip | nginx.conf... !"
 cp  /home/ubuntu/newbee.zip /bbs && cp  /home/ubuntu/nginx.conf/nginx.conf /bbs
-
+echo "unzip newbee.zip..."
 unzip -o /bbs/newbee.zip
 
 #docker cp /bbs/newbee/ nginx:/etc/nginx/html/
 
 #docker cp /bbs/nginx.config nginx:/etc/nginx/nginx.config
 
+# if you first run this script and behind the create-nginx-docker.sh maybe echo some error ,dont worry
 docker restart nginx
+echo "script complete !"
 
 
 

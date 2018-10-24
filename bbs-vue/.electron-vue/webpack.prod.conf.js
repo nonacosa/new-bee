83e9c4fa1ae373c122ed70ce06b3d5745608b71f
six.nonacosa <pkwenda@163.com>
2018-10-24 14:30:50 +0800

    引入 compression-webpack-plugin 开启 gzip 注意：2.0版本 asset - > filename

--- bbs-vue/.electron-vue/webpack.prod.conf.js
+++ bbs-vue/.electron-vue/webpack.prod.conf.js
@@ -104,7 +104,7 @@ if (config.build.productionGzip) {
 
   webpackConfig.plugins.push(
     new CompressionWebpackPlugin({
-      asset: '[path].gz[query]',
+      filename: '[path].gz[query]',
       algorithm: 'gzip',
       test: new RegExp(
         '\\.(' +

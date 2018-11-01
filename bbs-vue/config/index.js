b1bc21b9ef6f209ab930e8d37c8414888885f2c4
six.nonacosa <pkwenda@163.com>
2018-11-01 16:08:13 +0800

    webpack package path

--- bbs-vue/config/index.js
+++ bbs-vue/config/index.js
@@ -67,7 +67,7 @@ module.exports = {
         index: path.resolve(__dirname, '../dist/index.html'),
 
         // Paths
-        assetsRoot: path.resolve(__dirname, '../dist'),
+        assetsRoot: path.resolve(__dirname, '../dist/newbee'),
         assetsSubDirectory: 'static',
         assetsPublicPath: '/',
         assetsPublicConfig: './src/renderer/utils/configProd.js',
